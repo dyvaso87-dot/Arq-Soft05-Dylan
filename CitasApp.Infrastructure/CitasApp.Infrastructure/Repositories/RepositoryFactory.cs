9c1cdda3ff614e491b3d69a533052a612e0455a1
DylanTSW <dylan.vazquez@tecdesoftware.edu.mx>
2026-06-24 08:36:44 -0600

    feat: agregar proyectos CitasApp (Domain, Infrastructure)

---
+++ CitasApp.Infrastructure/CitasApp.Infrastructure/Repositories/RepositoryFactory.cs
@@ -0,0 +1,18 @@
+﻿using CitasApp.Domain.Interfaces;
+using CitasApp.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CitasApp.Infrastructure.Repositories
+{
+    public static class RepositoryFactory
+    {
+        public static IPacienteRepository CrearPacienteRepository(
+            string entorno, IWebHostEnvironment env)
+        {
+            if (entorno == "Production")
+                return new MemoriaPacienteRepository();
+
+            return new JsonPacienteRepository(env.ContentRootPath);
+        }
+    }
+}
